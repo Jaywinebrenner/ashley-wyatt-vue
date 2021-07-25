3c817738501c70199ebb491232c6ba9d50b38092
Jay Winebrenner <jaywinebrenner@gmail.com>
2021-07-25 09:40:33 -0700

    tweak

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# #!/usr/bin/env sh
+
+# set -e
+
+# npm run build
+
+# cd dist
+
+# git init 
+# git add -add
+# git commit -m 'New Deployment'
+# git push -f git@github.com:Jaywinebrenner/ashley-wyatt-vue.git master: gh-pages
+# cd -
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// import {defineConfig} from 'vite'
+// import vue from '@vite/plugin-vue'
+
+// export default defineConfig({
+//     base: '/ashley-wyatt-vue/',
+//     plugins: [vue()]
+// })
